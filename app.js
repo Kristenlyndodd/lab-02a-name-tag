d053fa8734cf0e5eddb66594190747afbc34ce09
kristenlynodd <kristenlyndodd@gmail.com>
2021-08-11 15:49:07 -0700

    README plan

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+// Change name text when clicking change name button
+
+// Change pronouns text when clicking the change pronouns button
+
+// Change background color of tag when button is clicked
+
+// Access to text input 
+
+//
